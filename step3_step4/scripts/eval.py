eea74b4efff78d32bc9d15b0dfcae01f44a0db6e
Gong Xiangbo <111922848+GongXiangbo@users.noreply.github.com>
2022-08-24 01:31:53 +0200

    Update eval.py

--- step3_step4/scripts/eval.py
+++ step3_step4/scripts/eval.py
@@ -8,50 +8,6 @@ import torch.cuda.amp as amp
 from utils.utils import reverse_one_hot, compute_global_accuracy, fast_hist, per_class_iu
 
 
-def val(args, model, dataloader):
-    print('start val!')
-    # label_info = get_label_info(csv_path)
-    with torch.no_grad():
-        model.eval()
-        precision_record = []
-        hist = np.zeros((args.num_classes, args.num_classes))
-        for index, batch in tqdm(enumerate(dataloader), disable=False):
-            data, label, _, name = batch
-            label = label.type(torch.LongTensor)
-            data = data.cuda()
-            label = label.long().cuda()
-            # label = torch.squeeze(label).long().cuda()
-
-            # get RGB predict image
-            predict = model(data).squeeze()
-            predict = reverse_one_hot(predict)
-            predict = np.array(predict.cpu())
-
-            # get RGB label image
-            label = label.squeeze()
-            if args.loss == 'dice':
-                label = reverse_one_hot(label)
-            label = np.array(label.cpu())
-
-            # compute per pixel accuracy
-            precision = compute_global_accuracy(predict, label)
-            hist += fast_hist(label.flatten(), predict.flatten(), args.num_classes)
-
-            # there is no need to transform the one-hot array to visual RGB array
-            # predict = colour_code_segmentation(np.array(predict), label_info)
-            # label = colour_code_segmentation(np.array(label), label_info)
-            precision_record.append(precision)
-        
-        precision = np.mean(precision_record)
-        miou_list = per_class_iu(hist)
-        miou = np.mean(miou_list)
-        print('precision per pixel for test: %.3f' % precision)
-        print('mIoU for validation: %.3f' % miou)
-        print(f'mIoU per class: {miou_list}')
-
-        return precision, miou
-
-
 def validate(args, model, val_loader, metrics, visualizer, val_iter):
     model.eval()
     metrics.reset()
